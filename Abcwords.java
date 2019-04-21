90f17b1968c48177a6f95717aa4dc6ccae8a1cac
AwpData <awpdata@gmail.com>
2019-04-20 17:59:10 -0700

    Create Abcwords.java

---
+++ Abcwords.java
@@ -0,0 +1,31 @@
+package CompSciCompetition;
+
+import java.io.*;
+import java.util.*;
+
+public class Abcwords {
+	public static void main(String[] args) throws FileNotFoundException {
+		PrintStream output = new PrintStream("src/output");
+		Scanner filereader = new Scanner(new File("D:/judge/abcwords.in"));
+		int num = Integer.parseInt(filereader.nextLine());
+
+		while (num > 0) {
+			boolean good = true;
+			String word = filereader.nextLine();
+			System.out.println(word);
+			int i = 0;
+			do {
+				char letter = word.charAt(i);
+				if (letter > word.charAt(i + 1)) {
+					output.println("NO");
+					good = false;
+				}
+				i++;
+			} while (good == true && i < word.length() - 1);
+			if(good == true) {
+			output.println("YES");
+			}
+			num--;
+		}
+	}
+}
