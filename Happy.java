eac149b31cbfa41a0e3bfcb381d8c06830921cf4
AwpData <awpdata@gmail.com>
2019-04-20 17:59:55 -0700

    Create Happy.java

---
+++ Happy.java
@@ -0,0 +1,14 @@
+package CompSciCompetition;
+
+import java.io.*;
+
+
+public class Happy {
+	public static void main(String[]args) throws FileNotFoundException {
+		PrintStream output = new PrintStream("src/output");
+		output.println("\t\t /\\_/\\");
+		output.println(" _______/ o o \\");
+		output.println("/~______  =o= /");
+		output.println("(_______)__m_m)");
+	}
+}
