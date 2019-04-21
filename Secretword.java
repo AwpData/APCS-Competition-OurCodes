5dc92f2aaeefb3067f5892bcfdb29c9d903303ca
AwpData <awpdata@gmail.com>
2019-04-20 17:57:04 -0700

    Create Secretword.java

---
+++ Secretword.java
@@ -0,0 +1,35 @@
+package CompSciCompetition;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Secretword {
+	public static void main(String[] args) throws FileNotFoundException {
+		PrintStream output = new PrintStream("src/output");
+		Scanner filereader = new Scanner(new File("D:/judge/secretword.in"));
+		int s = Integer.parseInt(filereader.nextLine());
+		while (s > 0) {
+			int n = Integer.parseInt(filereader.nextLine());
+			String line = filereader.nextLine();
+			Scanner splitter = new Scanner(line);
+			String out = "";
+			int count = 1;
+			while (count <= n) {
+				String word = splitter.next();
+				int len = word.length();
+				if (len >= count) {
+					if (len< count) {
+						out += word.substring(len -1, len);
+					} else {
+						out += word.substring(len-count,len-count+1);
+					}
+				}
+				count++;
+			}
+			output.println(out);
+			s--;
+		}
+	}
+}
