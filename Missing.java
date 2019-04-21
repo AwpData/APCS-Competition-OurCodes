a5155ec9b916ca71b83f94733c2f58a8975ea6dc
AwpData <awpdata@gmail.com>
2019-04-20 17:59:27 -0700

    Create Missing.java

---
+++ Missing.java
@@ -0,0 +1,29 @@
+package CompSciCompetition;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Missing {
+	public static void main(String[] args) throws FileNotFoundException {
+		PrintStream output = new PrintStream("src/output");
+		Scanner filereader = new Scanner(new File("D:/judge/missing.in"));
+		int n = Integer.parseInt(filereader.nextLine());
+		while(n>0) {
+			String line = filereader.nextLine();
+			Scanner splitter = new Scanner(line);
+			int min = splitter.nextInt();
+			int max = splitter.nextInt();
+			int total = splitter.nextInt();
+			int add = 0;
+			for(int i=min;i<=max;i++) {
+				add += i;
+			
+			}
+			add=add-total;
+			output.println(add);
+			n--;
+		}
+	}
+}
