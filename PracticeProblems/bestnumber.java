b159743c0629f49c1e7e993d465590e518b74a6e
AwpData <awpdata@gmail.com>
2019-04-23 08:56:24 -0700

    Create bestnumber.java

---
+++ PracticeProblems/bestnumber.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class bestnumber {
+	public static void main(String[] args) throws FileNotFoundException {
+		PrintStream output = new PrintStream("src/bestnumber/output");
+		Scanner filereader = new Scanner(new File("src/bestnumber/bestnumber"));
+		filereader.nextLine();
+		int big;
+		if (filereader.nextLine().equals("biggest")) {
+			big = 0;
+		} else {
+			big = 1;
+		}
+		double total = filereader.nextDouble();
+		while (filereader.hasNextDouble()) {
+			double num = filereader.nextDouble();
+			if (num >= 1) {
+				total *= num;
+			} else {
+				total /= num;
+			}
+		}
+		output.println(total);
+	}
+}
