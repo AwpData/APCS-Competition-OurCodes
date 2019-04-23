a9173fed3b7f969f553cc247cf4a91c59d2ac43f
AwpData <awpdata@gmail.com>
2019-04-23 08:07:03 -0700

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,8 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Knight {
 	public static void main(String[] args) throws FileNotFoundException {
