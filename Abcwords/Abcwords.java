e250564adcf05f06fba2e0f4fd10ce5499265257
AwpData <awpdata@gmail.com>
2019-04-23 21:08:59 -0700

    Update Abcwords.java

--- Abcwords/Abcwords.java
+++ Abcwords/Abcwords.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Abcwords {
 	public static void main(String[] args) throws FileNotFoundException {
 		PrintStream output = new PrintStream("src/output");
-		Scanner filereader = new Scanner(new File("D:/judge/abcwords.in"));
+		Scanner filereader = new Scanner(new File("src/abcwords.in"));
 		int num = Integer.parseInt(filereader.nextLine());
 
 		while (num > 0) {
