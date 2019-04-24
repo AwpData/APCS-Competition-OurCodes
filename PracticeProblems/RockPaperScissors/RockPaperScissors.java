ec975ef8f4cc5c74656df716d0546af991384714
AwpData <awpdata@gmail.com>
2019-04-23 21:10:44 -0700

    Update RockPaperScissors.java

--- PracticeProblems/RockPaperScissors/RockPaperScissors.java
+++ PracticeProblems/RockPaperScissors/RockPaperScissors.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class RockPaperScissors {
 	public static void main(String[]args) throws FileNotFoundException {
-		PrintStream output = new PrintStream("src/rock/output");
-		Scanner filereader = new Scanner(new File("src/rock/rock"));
+		PrintStream output = new PrintStream("src/output");
+		Scanner filereader = new Scanner(new File("src/rock.in"));
 		int num = Integer.parseInt(filereader.nextLine());
 		
 		while (num > 0) {
